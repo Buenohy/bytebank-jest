2cd6d453456646f3e0a9bfc3cc459708cd6504c3
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-20 08:55:17 -0300

    feat: test user render name

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,8 @@
-test('Primeiro teste', () => {
-  const numero = 10;
-  expect(numero).toBe(10);
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
+test('Deve renderizar o nome do usuário logado', () => {
+  render(<Cabecalho />);
+  const nomeUsuário = screen.getByText('Joana Fonseca Gomes');
+  expect(nomeUsuário).toBeInTheDocument();
 });
