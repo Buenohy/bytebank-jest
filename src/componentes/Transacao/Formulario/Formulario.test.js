38306a055c49e3c91f92ec1a629b2c1a3dae91c9
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-20 09:52:26 -0300

    feat: test Formulario render input with type number

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -6,3 +6,15 @@ test('Deve renderizar um campo de input', () => {
   const campoTexto = screen.getByPlaceholderText('Digite um valor');
   expect(campoTexto).toBeInTheDocument();
 });
+
+test('Deve renderizar um campo de input', () => {
+  render(<Formulario />);
+  const campoTexto = screen.getByPlaceholderText('Digite um valor');
+  expect(campoTexto).toBeInTheDocument();
+});
+
+test('Deve renderizar um campo de input com o type number', () => {
+  render(<Formulario />);
+  const campoTexto = screen.getByPlaceholderText('Digite um valor');
+  expect(campoTexto).toHaveAttribute('type', 'number');
+});
