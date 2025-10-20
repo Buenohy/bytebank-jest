cdd437f365aaf55067d23ea26c0236c04e9db5d7
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-20 09:57:46 -0300

    feat: describe with all tests

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -1,20 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Formulario from './index';
 
-test('Deve renderizar um campo de input', () => {
-  render(<Formulario />);
-  const campoTexto = screen.getByPlaceholderText('Digite um valor');
-  expect(campoTexto).toBeInTheDocument();
-});
+describe('Deve renderizar um campo de input', () => {
+  test('no documento', () => {
+    render(<Formulario />);
+    const campoTexto = screen.getByPlaceholderText('Digite um valor');
+    expect(campoTexto).toBeInTheDocument();
+  });
 
-test('Deve renderizar um campo de input', () => {
-  render(<Formulario />);
-  const campoTexto = screen.getByPlaceholderText('Digite um valor');
-  expect(campoTexto).toBeInTheDocument();
-});
+  test('de input com o type number', () => {
+    render(<Formulario />);
+    const campoTexto = screen.getByPlaceholderText('Digite um valor');
+    expect(campoTexto).toHaveAttribute('type', 'number');
+  });
 
-test('Deve renderizar um campo de input com o type number', () => {
-  render(<Formulario />);
-  const campoTexto = screen.getByPlaceholderText('Digite um valor');
-  expect(campoTexto).toHaveAttribute('type', 'number');
+  test('de input que pode ser preenchido', () => {
+    render(<Formulario />);
+    const campoTexto = screen.getByPlaceholderText('Digite um valor');
+    userEvent.type(campoTexto, '50');
+    expect(campoTexto).toHaveValue(50);
+  });
 });
